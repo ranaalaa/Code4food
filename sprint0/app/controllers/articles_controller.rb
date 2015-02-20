57c8defd66ccd2dfc5047bdc795c1a0d77358f5a
HananGHannam <hanan.ghannam93@gmail.com>
2015-02-20 12:46:12 +0200

    dah el gedeed

--- sprint0/app/controllers/articles_controller.rb
+++ sprint0/app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
 
   before_action :authenticate_user!, except: [:index, :show]
 
-  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+  #http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
 
     def index
     @articles = Article.all
